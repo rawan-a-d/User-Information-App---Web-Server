ece8785599009f6526022c321f983a4bf2c3cf30
Rawan Abou Dehn <rawan.ad7@gmail.com>
2017-05-23 22:09:15 +0200

    last commit

--- app.js
+++ app.js
@@ -59,7 +59,6 @@ app.post('/search', function (req, res) {
 //live search
 // Part 1: Autocomplete
 // Modify your form so that every time the user enters a key, it makes an AJAX call that populates the search results.
-// Do this work in a git branch called "autocomplete". Then, merge this branch into master with a pull request.
 app.post('/suggestionFinder', function(req,res){
 	var suggest = ''
 	fs.readFile('./resources/users.json', function (error, data) {
